a4f23d412d9f0568ad0389b24416c9803c2cff58
Punchh_ShashankY <punchh_shashanky@Punchh-ShashankYs-MacBook-Pro.local>
2021-11-21 12:56:47 +0530

    minor fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    def dashboard
+  def dashboard
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+    if resource.is_a?(User)
+      "/dashboard"
+    else
+      super
     end
+  end
+    
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
-  root "application#dashboard"
+
+  devise_scope :user do
+    root "users/sessions#new"
+  end
+
+  get "/dashboard", to: 'application#dashboard'
+
   get '/404', to: 'errors#not_found'
   get '/500', to: 'errors#internal_server'
   get '/422', to: 'errors#unprocessable'
